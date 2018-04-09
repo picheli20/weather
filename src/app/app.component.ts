a8fde5e96a4bafe2139ef4ba44a647ef3869f9a2
Fabio Picheli <contato@fabiopicheli.com>
2018-04-09 15:06:16 -0300

    fix: update card layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { WeatherService } from './weather/weather.service';
+
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'ca-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  list$ = this.weatherService.load([`3421319`, `3445709`, `184745`])
+    .pipe(
+      map(data => {
+        data.map(item => {
+          item.main.temp = Math.round(item.main.temp);
+          item.main.pressure = Math.round(item.main.pressure);
+        });
+        return data;
+      }),
+    );
+
+  constructor(public weatherService: WeatherService) {}
 }
