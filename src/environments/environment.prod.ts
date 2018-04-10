d7551ab0f6a3d9e8a47cf0daa89f0c3a6cd60a0a
Fabio Picheli <contato@fabiopicheli.com>
2018-04-10 20:45:15 -0300

    fix: production url and build task

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { EnvConfig } from '../app/core/environment.token';
 export const environment: EnvConfig = {
   production: true,
   api: {
-    url: `http://api.openweathermap.org/data/2.5/`,
+    url: `https://api.openweathermap.org/data/2.5/`,
     token: 'e41461f4abde3ad00782aba29fc8f8eb',
     unit: 'metric',
   },
