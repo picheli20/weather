d91e26cb06ed408e73410c7d8384b9013233aee1
Fabio Picheli <contato@fabiopicheli.com>
2018-04-10 20:25:42 -0300

    feat: add server script

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(path.join(__dirname, '../dist/index.html')));
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
